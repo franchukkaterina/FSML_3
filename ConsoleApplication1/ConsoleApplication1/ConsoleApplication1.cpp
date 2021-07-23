021831c02c636db28af987b0fc68946574b86e87
Fran_nd19@student.itstep.org <Fran_nd19@student.itstep.org>
2021-07-23 07:45:45 +0300

    init commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include<SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(400, 400), "Hello");
+	while (window.isOpen)
+	{
+
+	}
+    window.display();
+}
